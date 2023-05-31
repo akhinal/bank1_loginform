20707966b080d83c4a319ed1005ad2ab74c34f4d
Akhinal kv <akhinalkv573@gmail.com>
2023-05-31 20:49:09 +0530

    second

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
   data="Happy Banking With Us"
   pdata="Account Number"
+  servicedata:any
+  constructor(private rout:Router,private sd:DataService){}
+ngOnInit():void{
+
+  this.servicedata=this.sd.svar
+  console.log(this.servicedata);
+  this.sd.smethod()
+  
+}
+
+
   login(){
     alert("login is clicked")
+    this.rout.navigateByUrl('home')
+    
   }
- accno(event:any){
-console.log(event.target.value );
+//  accno(event:any){
+// console.log(event.target.value );
 
- } 
+//  } 
 }
